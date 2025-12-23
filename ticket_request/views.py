f4649bfa7f4730bc29d1f0a3d0f6caf0cfb65ad5
phanquockhanh <phanquockhanh@vnpt.vn>
2025-12-23 16:01:27 +0700

    k update views

--- ticket_request/views.py
+++ ticket_request/views.py
@@ -7,4 +7,7 @@ def ticket_request_view(request):
 
 
 def ticket_request_success_view(request):
-    return render(request, "ticket_request/ticket_request_success.html")
+    return render(request, "ticket_request/ticket_request_successed.html")
+
+def  ticket_request_detail_view(request):
+    return render(request, "ticket_request/ticket_request_detail.html")
